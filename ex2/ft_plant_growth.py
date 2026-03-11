faf717bbf6feb44033d587ac5c99ee5d87524fa0
dioppolo <diego.ioppolo2006@gmail.com>
2026-03-11 11:23:53 +0100

    ex2_new

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -4,19 +4,25 @@ class Plant:
         self.height = height
         self.age = age
 
-    def ft_garden_data(self):
+    def grow(self):
+        self.height += 2
+
+    def age_one_day(self):
+        self.age += 1
+
+    def get_info(self):
         print(f"{self.name}: {self.height}cm, {self.age} days old")
 
 
 h_rose = 10
-tmp1 = h_rose
-a_rose = 30
 h_cactus = 100
-tmp2 = h_cactus
-a_cactus = 120
 h_sunflower = 110
-tmp3 = h_sunflower
+a_rose = 30
+a_cactus = 120
 a_sunflower = 45
+tmp1 = h_rose
+tmp2 = h_cactus
+tmp3 = h_sunflower
 
 rose = Plant("Rose", h_rose, a_rose)
 cactus = Plant("Cactus", h_cactus, a_cactus)
@@ -28,25 +34,18 @@ tmp = [tmp1, tmp2, tmp3]
 if __name__ == "__main__":
     print("== Day 1 ===")
     for i in range(3):
-        plants[i].ft_garden_data()
+        plants[i].get_info()
 
     print("== Day 7 ===")
-    for i in range(7):
-        h_rose = h_rose + 1
-        h_cactus = h_cactus + 3
-        h_sunflower = h_sunflower + 2
+    for i in range(6):
+        for plant in plants:
+            plant.grow()
+            plant.age_one_day()
 
-    a_rose = a_rose + 6
-    a_cactus = a_cactus + 6
-    a_sunflower = a_sunflower + 6
-
-    rose = Plant("Rose", h_rose, a_rose)
-    cactus = Plant("Cactus", h_cactus, a_cactus)
-    sunflower = Plant("Sunflower", h_sunflower, a_sunflower)
-
-    plants = [rose, cactus, sunflower]
-    new_heights = [h_rose, h_cactus, h_sunflower]
-
+    h_rose = rose.height
+    h_cactus = cactus.height
+    h_sunflower = sunflower.height
+    new_height = [h_rose, h_cactus, h_sunflower]
     for i in range(3):
-        plants[i].ft_garden_data()
-        print(f"Growth this week: +{new_heights[i] - tmp[i]}cm")
+        plants[i].get_info()
+        print(f"Growth this week: +{new_height[i] - tmp[i]}cm")
